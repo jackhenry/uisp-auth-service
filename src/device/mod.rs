0ee1ca6378b301d8ec3736732a4acd61ef775944
Jack Henry <jack@jackerickson.net>
2024-02-26 13:40:24 -0800

    Removed residual code.

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,22 +1,3 @@
 mod model;
 
 pub use model::*;
-
-/* #[cfg(test)]
-mod tests {
-    use dotenvy::dotenv;
-
-    use self::uisp::UISP_INSTANCE;
-
-    use super::*;
-
-    #[actix_web::test]
-    async fn test_fetch_device_list() -> Result<(), ApiError> {
-        dotenv().ok();
-        let list = UISP_INSTANCE.fetch_device_list().await?;
-
-        // List should be non-empty
-        assert!(list.len() > 0);
-        Ok(())
-    }
-} */
